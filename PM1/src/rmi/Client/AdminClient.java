3b398be79af167b36a4b4ad065a179d5819431e1
Aron <aron.engineer1@gmail.com>
2015-06-05 21:49:26 -0400

    Non Returners file updated

--- PM1/src/rmi/Client/AdminClient.java
+++ PM1/src/rmi/Client/AdminClient.java
@@ -1,5 +1,7 @@
 package rmi.Client;
 
+import java.io.File;
+import java.io.FileWriter;
 import java.rmi.Naming;
 import java.rmi.RMISecurityManager;
 import java.util.Scanner;
@@ -8,74 +10,70 @@ import rmi.Interface.AdminInterface;
 import rmi.Interface.StudentInterface;
 
 public class AdminClient extends Client {
-	
+
 	static AdminInterface ConcordiaServer;
 	static AdminInterface OttawaServer;
 	static AdminInterface WaterlooServer;
-	static final String Concordia ="Concordia", Ottawa="Ottawa", Waterloo="Waterloo";
+	static final String Concordia = "Concordia", Ottawa = "Ottawa",
+			Waterloo = "Waterloo";
 	protected static String instituteName;
-	
+
 	public void InitializeServer() throws Exception {
 		System.setSecurityManager(new RMISecurityManager());
-		ConcordiaServer = (AdminInterface)Naming.lookup("rmi://localhost:1099/Concordia");		
-		OttawaServer = (AdminInterface)Naming.lookup("rmi://localhost:1099/Ottawa");
-		WaterlooServer = (AdminInterface)Naming.lookup("rmi://localhost:1099/Waterloo");	
+		ConcordiaServer = (AdminInterface) Naming
+				.lookup("rmi://localhost:1099/Concordia");
+		OttawaServer = (AdminInterface) Naming
+				.lookup("rmi://localhost:1099/Ottawa");
+		WaterlooServer = (AdminInterface) Naming
+				.lookup("rmi://localhost:1099/Waterloo");
 	}
-	
-	public AdminInterface ServerValidation(Scanner keyboard)
-	{
+
+	public AdminInterface ServerValidation(Scanner keyboard) {
 		Boolean valid = false;
 		AdminInterface server = null;
 		System.out.println("Enter Institute Name");
 		System.out.println("'Concordia' For Concordia University");
 		System.out.println("'Ottawa' For Ottawa University");
 		System.out.println("'Waterloo' For Waterloo University");
-		while(!valid)
-		{
-			try{
+		while (!valid) {
+			try {
 				instituteName = keyboard.nextLine();
 				server = LocateServer(instituteName);
-				if(server != null) {
-					valid=true;
-				}
-				else {
+				if (server != null) {
+					valid = true;
+				} else {
 					System.out.println("Invalid Institute Name");
 					keyboard.nextLine();
 				}
-			}
-			catch(Exception e)
-			{
+			} catch (Exception e) {
 				System.out.println("Invalid Institute Name");
-				valid=false;
+				valid = false;
 				keyboard.nextLine();
 			}
 		}
-		//keyboard.nextLine();
+		// keyboard.nextLine();
 		return server;
 	}
-	
-	//Get Server Connection
+
+	// Get Server Connection
 	public static AdminInterface LocateServer(String instituteName) {
-		if(instituteName.equals(Concordia)) {
+		if (instituteName.equals(Concordia)) {
 			return ConcordiaServer;
-		}
-		else if(instituteName.equals(Ottawa)) {
+		} else if (instituteName.equals(Ottawa)) {
 			return OttawaServer;
-		}
-		else if(instituteName.equals(Waterloo)) {
+		} else if (instituteName.equals(Waterloo)) {
 			return WaterlooServer;
 		}
 		return null;
 	}
 
-	public static void showMenu()
-	{
+	public static void showMenu() {
 		System.out.println("DRMS Admin Client System \n");
 		System.out.println("Please select an option");
 		System.out.println("1. Get Non-Returner.");
 		System.out.println("2. Exit");
 	}
-	
+
 	public static void main(String[] args)
 	{
 		try{
@@ -112,7 +110,10 @@ public class AdminClient extends Client {
 					objClient.logger.info("Non Returner retrieved on :"+ System.currentTimeMillis());
 					String result = "";
 					result = objServer.getNonReturners(userName, password, objServer.toString(), numOfDays);
-					System.out.println(result);
+					File nonReturners=new File(".\\src\\NonReturners.txt");
+					FileWriter fw=new FileWriter(nonReturners);
+					fw.write(result);
+					System.out.println("File Written");
 					showMenu();
 					break;
 				case 2:
